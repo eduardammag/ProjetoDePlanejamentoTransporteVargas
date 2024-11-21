de35d7b0f44846305d4172f4c9c85e09198ef522
Eduarda Mesquita <eduardammag@gmail.com>
2024-11-20 21:10:13 -0300

    testando as classes

--- classes.h
+++ classes.h
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <random>
 #include <cmath>
-#include <ctime>
 using namespace std;
 
 class Vertex {
@@ -34,16 +33,16 @@ public:
     Edge(int distance, Vertex* vertex1, Vertex* vertex2, float trafficRate)
         : m_distance(distance), m_vertex1(vertex1), m_vertex2(vertex2), m_trafficRate(trafficRate) {
         
-        m_excavationCost = distance / 2.0f;        
+        m_excavationCost = distance / 2.0f;
         m_zipCode = generateRandomCode(4);  
         m_street = generateRandomCode(8);  
     }
- 
+
     int generateRandomCode(int numDigits) {
         random_device rd;  
-        mt19937 gen(rd());
-        uniform_int_distribution<int> dist(pow(10, numDigits - 1), pow(10, numDigits) - 1);
-        return dist(gen); 
+        mt19937 gen(rd());  
+        uniform_int_distribution<int> dist(pow(10, numDigits - 1), pow(10, numDigits) - 1);  
+        return dist(gen);  
     }
 
     Vertex* vertex1() {
@@ -74,3 +73,27 @@ public:
         return m_excavationCost;
     }
 };
+
+int main() {
+    Vertex v1(true, 1);  
+    Vertex v2(false, 2);  
+
+    Edge e(100, &v1, &v2, 1.5);  
+
+    cout << "TESTE DAS CLASSES" << endl;
+    cout << "-----------------" << endl;
+
+    cout << "Vértice 1 ID: " << e.vertex1()->id() << " | É estação de metrô? " 
+         << (e.vertex1()->isMetroStation() ? "Sim" : "Não") << endl;
+
+    cout << "Vértice 2 ID: " << e.vertex2()->id() << " | É estação de metrô? " 
+         << (e.vertex2()->isMetroStation() ? "Sim" : "Não") << endl;
+
+    cout << "Distância da aresta: " << e.distance() << endl;
+    cout << "CEP (4 dígitos): " << e.id_zipCode() << endl;
+    cout << "Rua (8 dígitos): " << e.id_street() << endl;
+    cout << "Taxa de Tráfego: " << e.trafficRate() << endl;
+    cout << "Custo de Escavação: " << e.excavationCost() << endl;
+
+    return 0;
+}
