7082be94e23443579f3a52ed1841ebbd14fa92e6
Ana Júlia Amaro <126619273+AnaJAPR@users.noreply.github.com>
2024-11-21 15:36:21 -0300

    adiciona teste da classe "graph"

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "classes.h"
+#include "graph.h"
 #include <random>
 #include <cmath>
 using namespace std;
@@ -11,7 +12,7 @@ int main() {
 
     Edge e(100, &v1, &v2, 1.5);  
 
-    cout << "TESTE DAS CLASSES" << endl;
+    cout << "TESTE DAS CLASSES VERTEX E EDGE" << endl;
     cout << "-----------------" << endl;
 
     cout << "Vértice 1 ID: " << e.vertex1()->id() << " | É estação de metrô? " 
@@ -25,6 +26,30 @@ int main() {
     cout << "Rua (8 dígitos): " << e.id_street() << endl;
     cout << "Taxa de Tráfego: " << e.trafficRate() << endl;
     cout << "Custo de Escavação: " << e.excavationCost() << endl;
+
+    // teste da construção do grafo e representação em lista e matriz de adjacência
+    
+    Graph g;
+
+    // Adiciona vértices
+    g.addVertex(true, 0);
+    g.addVertex(false, 1);
+    g.addVertex(true, 2);
+
+    // Adiciona arestas
+    g.addEdge(0, 1, 10, 1.2f);
+    g.addEdge(1, 2, 15, 0.8f);
+    g.addEdge(2, 2, 20, 1.0f);   // Testando Laço (não deve ser adicionado)
+
+    // Gera as representações
+    g.generateAdjacencyList();
+    g.generateAdjacencyMatrix();
+
+    // Imprime as representações
+    cout << "TESTE CLASSE GRAPH" << endl;
+    cout << "-----------------" << endl;
+    g.printAdjacencyList();
+    g.printAdjacencyMatrix();
 
     return 0;
 }
