0e506a13483d0a0275c146174bef6a173c58f930
Ana Júlia Amaro <126619273+AnaJAPR@users.noreply.github.com>
2024-11-25 14:47:15 -0300

    adiciona função pra agrupar arestas por região

--- buildGraph.cpp
+++ buildGraph.cpp
@@ -1,74 +1,78 @@
 #include "buildGraph.h"
 #include "vertexAndEdge.h"
 #include <fstream>
-#include <stdexcept>
 #include <iostream>
+#include <unordered_map>
 
-void parseJsonFile(const std::string& filePath, std::vector<Vertex*>& vertices, std::vector<Edge*>& edges) {
-    std::ifstream file(filePath);
+using namespace std;
+
+void parseJsonFile(const string& filePath, vector<Vertex*>& vertices, vector<Edge*>& edges) {
+    ifstream file(filePath);
     if (!file.is_open()) {
-        throw std::runtime_error("Erro ao abrir o arquivo JSON: " + filePath);
+        throw runtime_error("Erro ao abrir o arquivo JSON: " + filePath);
     }
 
-    std::string line;
+    string line;
     bool parsingVertices = false, parsingEdges = false;
 
-    while (std::getline(file, line)) {
+    while (getline(file, line)) {
         line.erase(0, line.find_first_not_of(" \t"));
         line.erase(line.find_last_not_of(" \t") + 1);
 
-        if (line.find("\"vertex\":") != std::string::npos) {
+        if (line.find("\"vertex\":") != string::npos) {
             parsingVertices = true;
             parsingEdges = false;
             continue;
         }
-        if (line.find("\"edges\":") != std::string::npos) {
+        if (line.find("\"edges\":") != string::npos) {
             parsingVertices = false;
             parsingEdges = true;
             continue;
         }
 
-        if (line.find("]") != std::string::npos) {
+        if (line.find("]") != string::npos) {
             parsingVertices = false;
             parsingEdges = false;
             continue;
         }
 
-        if (parsingVertices && line.find("{") != std::string::npos) {
+        if (parsingVertices && line.find("{") != string::npos) {
             int id = -1;
             bool isMetro = false;
 
-            while (std::getline(file, line) && line.find("}") == std::string::npos) {
+            while (getline(file, line) && line.find("}") == string::npos) {
                 line.erase(0, line.find_first_not_of(" \t"));
                 line.erase(line.find_last_not_of(" \t") + 1);
 
-                if (line.find("\"id_vertex\":") != std::string::npos) {
-                    id = std::stoi(line.substr(line.find(":") + 1));
-                } else if (line.find("\"isMetroStation\":") != std::string::npos) {
-                    isMetro = line.find("true") != std::string::npos;
+                if (line.find("\"id_vertex\":") != string::npos) {
+                    id = stoi(line.substr(line.find(":") + 1));
+                } else if (line.find("\"isMetroStation\":") != string::npos) {
+                    isMetro = line.find("true") != string::npos;
                 }
             }
             vertices.push_back(new Vertex(isMetro, id));
         }
 
-        if (parsingEdges && line.find("{") != std::string::npos) {
+        if (parsingEdges && line.find("{") != string::npos) {
             int idEdge = -1, v1 = -1, v2 = -1, distance = 0;
-            float trafficRate = 0.0f;
+            float trafficRate = 0.0f; int id_zipCode = 0;
 
-            while (std::getline(file, line) && line.find("}") == std::string::npos) {
+            while (getline(file, line) && line.find("}") == string::npos) {
                 line.erase(0, line.find_first_not_of(" \t"));
                 line.erase(line.find_last_not_of(" \t") + 1);
 
-                if (line.find("\"id_edge\":") != std::string::npos) {
-                    idEdge = std::stoi(line.substr(line.find(":") + 1));
-                } else if (line.find("\"v1\":") != std::string::npos) {
-                    v1 = std::stoi(line.substr(line.find(":") + 1));
-                } else if (line.find("\"v2\":") != std::string::npos) {
-                    v2 = std::stoi(line.substr(line.find(":") + 1));
-                } else if (line.find("\"distance\":") != std::string::npos) {
-                    distance = std::stoi(line.substr(line.find(":") + 1));
-                } else if (line.find("\"traficRate\":") != std::string::npos) {
-                    trafficRate = std::stof(line.substr(line.find(":") + 1));
+                if (line.find("\"id_edge\":") != string::npos) {
+                    idEdge = stoi(line.substr(line.find(":") + 1));
+                } else if (line.find("\"v1\":") != string::npos) {
+                    v1 = stoi(line.substr(line.find(":") + 1));
+                } else if (line.find("\"v2\":") != string::npos) {
+                    v2 = stoi(line.substr(line.find(":") + 1));
+                } else if (line.find("\"distance\":") != string::npos) {
+                    distance = stoi(line.substr(line.find(":") + 1));
+                } else if (line.find("\"traficRate\":") != string::npos) {
+                    trafficRate = stof(line.substr(line.find(":") + 1));
+                } else if (line.find("\"cep\":") != string::npos) {
+                    id_zipCode = stof(line.substr(line.find(":") + 1));
                 }
             }
 
@@ -81,7 +85,7 @@ void parseJsonFile(const std::string& filePath, std::vector<Vertex*>& vertices, 
             }
 
             if (vertex1 && vertex2) {
-                Edge* edge = new Edge(distance, vertex1, vertex2, trafficRate, idEdge);
+                Edge* edge = new Edge(distance, vertex1, vertex2, trafficRate, idEdge, id_zipCode);
                 edges.push_back(edge);
             }
         }
@@ -90,31 +94,59 @@ void parseJsonFile(const std::string& filePath, std::vector<Vertex*>& vertices, 
     file.close();
 }
 
-void generateAdjacencyMatrix(const std::vector<Vertex*>& vertices, const std::vector<Edge*>& edges, std::vector<std::vector<Edge*>>& adjacencyMatrix) {
+void generateAdjacencyMatrix(const vector<Vertex*>& vertices, const vector<Edge*>& edges, vector<vector<Edge*>>& adjacencyMatrix) {
     int n = vertices.size();
-    adjacencyMatrix.resize(n, std::vector<Edge*>(n, nullptr));  // Inicializa com nullptr
+    adjacencyMatrix.resize(n, vector<Edge*>(n, nullptr));  //Inicializa com nullptr
 
     // Preenche a matriz de adjacência com ponteiros para as arestas
     for (const auto& edge : edges) {
         int v1_id = edge->vertex1()->id();
         int v2_id = edge->vertex2()->id();
         
-        // Grafo não direcionado, então preenche ambos os lugares
+        //Grafo não direcionado, então preenche ambos os lugares
         adjacencyMatrix[v1_id][v2_id] = edge;
         adjacencyMatrix[v2_id][v1_id] = edge;
     }
 }
 
-void generateAdjacencyList(const std::vector<std::vector<Edge*>>& adjacencyMatrix, std::vector<std::vector<std::tuple<int, Edge*>>>& adjacencyList) {
+void generateAdjacencyList(const vector<vector<Edge*>>& adjacencyMatrix, vector<vector<tuple<int, Edge*>>>& adjacencyList) {
     int n = adjacencyMatrix.size();
-    adjacencyList.resize(n);  // Inicializa a lista de adjacência
+    adjacencyList.resize(n);  //Inicializa a lista de adjacência
 
-    // Preenche a lista de adjacência com tuplas (ID do vértice de destino, ponteiro para a aresta)
+    //Preenche a lista de adjacência com tuplas (ID do vértice de destino, ponteiro para a aresta)
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (adjacencyMatrix[i][j] != nullptr) {  // Verifica se existe uma aresta
-                adjacencyList[i].push_back(std::make_tuple(j, adjacencyMatrix[i][j]));
+            if (adjacencyMatrix[i][j] != nullptr) {  //Verifica se existe uma aresta
+                adjacencyList[i].push_back(make_tuple(j, adjacencyMatrix[i][j]));
             }
         }
+    }
+}
+
+vector<vector<Edge*>> groupEdgesByCepVector(const vector<Edge*>& edges) {
+    //Mapeia os `ceps` para listas de arestas
+    unordered_map<int, vector<Edge*>> cepToEdgesMap;
+
+    for (const auto& edge : edges) {
+        int cep = edge->id_zipCode();
+        cepToEdgesMap[cep].push_back(edge);
+    }
+
+    //Converte o mapa em um vetor de listas, preservando os grupos por `cep`
+    vector<vector<Edge*>> edgesByCep;
+    for (const auto& [cep, edgeList] : cepToEdgesMap) {
+        edgesByCep.push_back(edgeList);
+    }
+
+    return edgesByCep;
+}
+
+void printEdgesGroupedByCepVector(const unordered_map<int, vector<Edge*>>& cepToEdgesMap) {
+    for (const auto& [cep, edgeList] : cepToEdgesMap) {
+        cout << "CEP: " << cep << " -> Arestas: ";
+        for (const auto& edge : edgeList) {
+            cout << edge->idEdge() << " "; //Imprime o ID da aresta
+        }
+        cout << endl;
     }
 }
