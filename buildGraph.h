48181239037a3521942701b0caad4fa2b9839316
Ana Júlia Amaro <126619273+AnaJAPR@users.noreply.github.com>
2024-11-25 14:48:05 -0300

    adiciona assinatura da função que agrupa arestas por cep

--- buildGraph.h
+++ buildGraph.h
@@ -6,13 +6,21 @@
 #include <tuple>
 #include "vertexAndEdge.h"
 
-// Função para fazer o parse do arquivo JSON e preencher os vetores de vértices e arestas
-void parseJsonFile(const std::string& filePath, std::vector<Vertex*>& vertices, std::vector<Edge*>& edges);
+using namespace std;
 
-// Função para gerar a matriz de adjacência
-void generateAdjacencyMatrix(const std::vector<Vertex*>& vertices, const std::vector<Edge*>& edges, std::vector<std::vector<Edge*>>& adjacencyMatrix);
+//Função para fazer o parse do arquivo JSON e preencher os vetores de vértices e arestas
+void parseJsonFile(const string& filePath, vector<Vertex*>& vertices, vector<Edge*>& edges);
 
-// Função para converter a matriz de adjacência para lista de adjacência
-void generateAdjacencyList(const std::vector<std::vector<Edge*>>& adjacencyMatrix, std::vector<std::vector<std::tuple<int, Edge*>>>& adjacencyList);
+//Função para gerar a matriz de adjacência
+void generateAdjacencyMatrix(const vector<Vertex*>& vertices, const vector<Edge*>& edges, vector<vector<Edge*>>& adjacencyMatrix);
 
-#endif // BUILDGRAPH_H
+//Função para converter a matriz de adjacência para lista de adjacência
+void generateAdjacencyList(const vector<vector<Edge*>>& adjacencyMatrix, vector<vector<tuple<int, Edge*>>>& adjacencyList);
+
+//Função para gerar um vetor de listas de arestas agrupadas por cep
+vector<vector<Edge*>> groupEdgesByCepVector(const vector<Edge*>& edges);
+
+//Função auxiliar para imprimir as listas de arestas para cada cep
+void printEdgesGroupedByCepVector(const unordered_map<int, vector<Edge*>>& cepToEdgesMap);
+
+#endif
